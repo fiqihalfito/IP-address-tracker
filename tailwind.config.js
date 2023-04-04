9e2093d9cf8a7222b88b91c575388b04b2683d8f
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-04-04 16:22:48 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["var(--font-rubik)"]
+      }
+    },
+  },
+  plugins: [],
+}
+
